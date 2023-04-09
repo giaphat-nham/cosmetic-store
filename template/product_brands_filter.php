14654b87a9a256716c87a3a10c3f4c929e44b823
giaphat-nham <phatnham3010@gmail.com>
2023-04-09 19:43:47 +0700

    Tach file thanh nhieu file PHP

---
+++ template/product_brands_filter.php
@@ -0,0 +1,15 @@
+<div class="product-brands-filter radio-button-group">
+    <label for="brand" class="label">Thương hiệu:</label>
+    <div class="radio-option">
+        <input type="radio" name="brand" id="cocoon" value="cocoon">
+        <label for="face-care">Cocoon</label>
+    </div>
+    <div class="radio-option">
+        <input type="radio" name="brand" id="bioderma" value="bioderma">
+        <label for="body-care">Bioderma</label>
+    </div>
+    <div class="radio-option">
+        <input type="radio" name="brand" id="La Roche Posay" value="La Roche Posay">
+        <label for="makeup">La Roche Posay</label>
+    </div>
+</div>
