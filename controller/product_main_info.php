566ba147d03f7dea53e57a7d6e12a3d7bd9ad453
giaphat-nham <phatnham3010@gmail.com>
2023-05-16 14:48:46 +0700

    Thong bao them gio hang

--- controller/product_main_info.php
+++ controller/product_main_info.php
@@ -12,6 +12,7 @@
                 xmlhttp.onload = function() {
                     const basket =document.querySelector("#btn-basket");
                     basket.dataset.totalProduct = this.responseText;
+                    alert("Thêm sản phẩm thành công vào giỏ hàng!");
                 };
                 xmlhttp.open("GET","./model/update_cart.php?productId="+productId+"&accountId="+accountId);
                 xmlhttp.send();
